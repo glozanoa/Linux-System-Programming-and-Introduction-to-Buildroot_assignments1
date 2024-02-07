5333a92dd8baea311d227f1fe8f95228ea500289
Gustavo Lozano <glozanoac@gmail.com>
2024-02-07 10:05:43 -0500

    trigger github action workflow (6)

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Search an string recursively (on files in a directory)
+# search an string recursively (on files in a directory)
 
 if [ $# -lt 2 ];then
   echo "USAGE: $0 <filesdir> <searchstr>"
